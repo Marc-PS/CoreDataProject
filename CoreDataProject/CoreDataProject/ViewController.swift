ab4ff0d6732d467e15d56945db8db99a7927db1a
Marc Perelló Sapiña <marc.perellosapina@gmail.com>
2021-02-05 19:26:45 +0100

    V.0

---
+++ CoreDataProject/CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Marc Perelló Sapiña on 5/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
